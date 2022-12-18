c81c0e21804c1a18203af0a39b3d0f47150ed042
Samantha Burak <samantha.burak@gmail.com>
2022-12-18 00:31:00 -0500

    Fixed post spacing and adding shadows, icons, etc.

--- components/Post.js
+++ components/Post.js
@@ -1,70 +1,91 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Button } from 'react-native';
+import { Ionicons, MaterialCommunityIcons} from '@expo/vector-icons'; 
 
 export default function Post() {
   return (
-    <View style={styles.fits}>
-      <TouchableOpacity style={styles.usernamebutton} onPress={null}>
-            <Text style={styles.usernametext}>Username</Text>
+    <View style={styles.post}>
+      <TouchableOpacity style={[styles.usernamebutton, styles.shadowProp]} onPress={null}>
+          <Ionicons name="person" size={20}/> 
+          <Text style={styles.usernametext}>{'  '}Username</Text>
         </TouchableOpacity> 
-      <Image
-        style={styles.image}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-      <View style={styles.tags}>
-        <Text style={styles.tag}>Style</Text>
-        <Text style={styles.tag}>Activity</Text>
+      <View style={styles.centered}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <View style={styles.tags}>
+          <View style={[styles.shadowProp, styles.tag]}>
+            <MaterialCommunityIcons name="briefcase-outline" size={14}/>
+            <Text>{' '}Style</Text>
+          </View>
+          <View style={[styles.shadowProp, styles.tag]}>
+            <Ionicons name="md-globe-outline" size={14}/>
+            <Text>{' '}Activity</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  fits: {
+  post: {
     borderColor: '#000',
     borderWidth: 1,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'stretch'
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  shadowProp: {
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    }
   },
   usernamebutton: {
-    alignItems: 'center',
-    paddingTop: 16,
-    paddingLeft: 30,
-    paddingRight: 30,
-    paddingBottom: 16,
-    borderColor: '#000',
-    borderWidth: 2,
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 10,
     borderRadius: 32,
-    marginBottom: 8,
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    marginLeft: 24,
+    backgroundColor: '#fff',
+    alignItems: 'center'
+  },
+  centered: {
+    alignItems: 'center',
   },
   usernametext: { 
     fontSize: 16,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 250,
+    height: 250,
   },
   tags: {
-    marginTop: 8,
     marginBottom: 8,
-    paddingLeft: 10,
-    paddingRight: 10,
-    borderColor: '#000',
-    borderWidth: 2,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    backgroundColor: '#fff',
   },
   tag: {
-    borderColor: '#000',
-    borderWidth: 1,
-    paddingTop: 6,
-    paddingBottom: 6,
-    paddingLeft: 20,
-    paddingRight: 20,
+    paddingTop: 12,
+    paddingBottom: 12,
+    justifyContent: 'center',
     marginTop: 4,
     marginBottom: 4,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 150,
   }
 });
